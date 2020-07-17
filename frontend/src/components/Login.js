598591239d8c0a0c8e287db8a5dacb9ea55df056
Jacques Jordaan <5298218+zoidbergZA@users.noreply.github.com>
2020-07-17 21:32:48 +0200

    auth state persistence

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,29 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import app from '../base';
+import { AuthContext } from '../contexts/Auth';
 import Button from 'react-bulma-components/lib/components/button';
 import Heading from 'react-bulma-components/lib/components/heading';
+import { Redirect } from 'react-router';
 
 import * as firebase from 'firebase/app';
 
-const Login = () => {
+const Login = ({ history }) => {
   const loginClickHandler = () => {
     var provider = new firebase.auth.GithubAuthProvider();
 
-    app.auth().signInWithPopup(provider).then(function(result) {
-      // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-      var token = result.credential.accessToken;
-      console.log(token);
+    app.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL).then(() => {
+      app.auth().signInWithPopup(provider).then(function(result) {
+        history.push('/');
+      }).catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
 
-      // The signed-in user info.
-      var user = result.user;
-      console.log(user);
-    }).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-
-      console.log(`${errorCode} :: ${errorMessage}`);
+        console.log(`${errorCode} :: ${errorMessage}`);
+      });
     });
+  }
+
+  const { currentUser } = useContext(AuthContext);
+
+  if (currentUser) {
+    return <Redirect to="/" />;
   }
 
   return (

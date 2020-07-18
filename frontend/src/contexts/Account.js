e8b7c3a63207585e6b48354ea39080be810660c4
Jacques Jordaan <5298218+zoidbergZA@users.noreply.github.com>
2020-07-18 18:27:58 +0200

    cleanup

--- frontend/src/contexts/Account.js
+++ frontend/src/contexts/Account.js
@@ -1,37 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import app from '../base';
-import { authState } from 'rxfire/auth';
 import { doc } from 'rxfire/firestore';
-import { switchMap, map } from 'rxjs/operators';
-import { from } from 'rxjs';
+import { AuthContext } from 'contexts/Auth';
 
 export const TurtleAccountContext = React.createContext();
 
 export const TurtleAccountProvider = ({ children }) => {
+  const { currentUser } = useContext(AuthContext);
   const [turtleAccount, setTurtleAccount] = useState(null);
 
   useEffect(() => {
-    authState(app.auth())
-    .pipe(
-      switchMap(u => {
-        if (u) {
-          return doc(app.firestore().doc(`users/${u.uid}`));
-        }
-        return from([]);
-      }),
-      map(userDoc => userDoc.data()),
-      switchMap(user => {
-        if (user && user.accountId) {
-          return doc(app.firestore().doc(`accounts/${user.accountId}`));
-        } else {
-          return from([]);
-        }
-      }),
-      map(accountDoc => accountDoc.data())
-    ).subscribe(account => {
-      setTurtleAccount(account);
-    });
-  }, []);
+    if (currentUser && currentUser.accountId) {
+      doc(app.firestore().doc(`accounts/${currentUser.accountId}`)).subscribe(accountDoc => {
+        const account = accountDoc.data();
+        setTurtleAccount(account);
+      })
+    } else {
+      setTurtleAccount(null);
+    }
+  }, [currentUser]);
 
   return (
     <TurtleAccountContext.Provider value={{ turtleAccount }}>

662a7f99e9c171aed0c7fe53ce93b88863a6539b
Jacques Jordaan <5298218+zoidbergZA@users.noreply.github.com>
2020-07-19 20:34:11 +0200

    added copy to clipboard package

--- frontend/src/components/CopyBox/CopyBox.js
+++ frontend/src/components/CopyBox/CopyBox.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import copyBox from './copyBox.scss'
-import Button from 'react-bulma-components/lib/components/button';
 import Icon from 'react-bulma-components/lib/components/icon';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCopy } from '@fortawesome/free-solid-svg-icons';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const CopyBox = ({ data }) => {
+
+  const copyAddressHandler = () => {
+    console.log('copied!');
+  }
+
   return (
     <div className="data-box">
       <div className="data-item">
         <p>{data}</p>
       </div>
       <div className="copy-btn">
-      <Icon>
-        <FontAwesomeIcon icon={faCopy} />
-    </Icon>
+      <CopyToClipboard text={data} onCopy={copyAddressHandler}>
+        <Icon>
+          <FontAwesomeIcon icon={faCopy} />
+        </Icon>
+      </CopyToClipboard>
       </div>
     </div>
   )

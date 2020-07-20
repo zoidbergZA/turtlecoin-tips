eefceaf79f98d86be711293c92eb5656d2b7a61a
Jacques Jordaan <5298218+zoidbergZA@users.noreply.github.com>
2020-07-20 22:09:31 +0200

    added spinner component

---
+++ frontend/src/components/Spinner/Spinner.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Loader from 'react-bulma-components/lib/components/loader';
+
+
+const Spinner = ({ message }) => {
+  return (
+    <React.Fragment>
+      <Loader
+        style={{
+          display: "inline-block",
+          width: "64px",
+          height: "64px",
+          border: "4px solid grey",
+          borderTopColor: 'transparent',
+          borderRightColor: 'transparent' }} />
+      {message ? <p>{message}</p> : null}
+    </React.Fragment>
+  );
+};
+
+export default Spinner;

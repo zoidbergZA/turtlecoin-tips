c084e8a36252a8a5b973e315441550a7e4aabfd9
Jacques Jordaan <5298218+zoidbergZA@users.noreply.github.com>
2020-07-24 22:40:31 +0200

    copy box feedback

--- frontend/src/components/CopyBox/CopyBox.js
+++ frontend/src/components/CopyBox/CopyBox.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styles from './copyBox.module.scss'
 import Icon from 'react-bulma-components/lib/components/icon';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCopy } from '@fortawesome/free-solid-svg-icons';
+import { faCopy, faCheck } from '@fortawesome/free-solid-svg-icons';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const CopyBox = ({ data }) => {
+  const [copied, setCopied] = useState(false);
+  const timerRef = useRef(null);
 
   const copyAddressHandler = () => {
-    console.log('copied!');
+    setCopied(true);
+
+    timerRef.current = setTimeout(() => {
+      setCopied(false);
+    }, 2000);
   }
+
+  useEffect(() => {
+    return () => clearTimeout(timerRef.current)
+  }, []);
+
+  const iconStyles = copied ? [styles.green] : [];
 
   return (
     <div className={styles.box}>
@@ -19,7 +31,7 @@ const CopyBox = ({ data }) => {
       <div className={styles.btn}>
       <CopyToClipboard text={data} onCopy={copyAddressHandler}>
         <Icon>
-          <FontAwesomeIcon icon={faCopy} />
+          <FontAwesomeIcon className={iconStyles.join(' ')} icon={copied ? faCheck : faCopy} />
         </Icon>
       </CopyToClipboard>
       </div>

e1050fde25433aff8c7ac6245e7955c7a76feb6f
Jacques Jordaan <5298218+zoidbergZA@users.noreply.github.com>
2020-07-27 13:33:44 +0200

    cleanup

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,7 @@ import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 import * as webAppModule from './modules/webAppModule';
 import * as githubModule from './modules/github/githubModule';
+import * as coreModule from './modules/core/coreModule';
 import { TrtlApp } from 'trtl-apps';
 
 const turtleConfig = functions.config().trtl;
@@ -12,6 +13,6 @@ admin.initializeApp();
 // Init TRTL App
 TrtlApp.initialize(turtleConfig.app_id, turtleConfig.app_secret);
 
+export const core   = coreModule;
 export const webApp = webAppModule;
 export const github = githubModule;
-export { trtlAppsWebhook } from './modules/core/coreModule';

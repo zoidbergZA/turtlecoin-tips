8e4959c71742e916ca941b0ff1b9e1b46beb67de
Jacques Jordaan <5298218+zoidbergZA@users.noreply.github.com>
2020-09-12 21:03:32 +0200

    added material UI and moment packages

--- frontend/src/components/History/History.js
+++ frontend/src/components/History/History.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
+import { makeStyles } from '@material-ui/core';
+import Moment from 'react-moment';
 import { combineLatest } from 'rxjs';
 import { mergeAll } from 'rxjs/operators';
 import app from '../../base';
@@ -8,12 +10,18 @@ import Transaction from './Transaction/Transaction';
 import Heading from 'react-bulma-components/lib/components/heading';
 import Container from 'react-bulma-components/lib/components/container';
 import Section from 'react-bulma-components/lib/components/section';
-import Table from 'react-bulma-components/lib/components/table';
 import Spinner from '../Spinner/Spinner';
+
+const useStyles = makeStyles({
+  txItem: {
+    marginTop: '15px'
+  }
+});
 
 const History = () => {
   const { currentUser } = useContext(AuthContext);
   const [transactions, setTransactions] = useState(null);
+  const classes = useStyles();
 
   useEffect(() => {
     if (currentUser && currentUser.primaryAccountId) {
@@ -39,15 +47,30 @@ const History = () => {
   let history;
 
   if (transactions) {
-    history = (
-      <Table>
-        <tbody>
-          {transactions.map(tx => <Transaction key={tx.id} tx={tx}></Transaction>)}
-        </tbody>
-      </Table>
-    )
+    const items = [];
+
+    for (const [index, tx] of transactions.entries()) {
+      if (index === 0) {
+        items.push(<Moment unix key={index} format="MMMM D, YYYY">{tx.timestamp / 1000}</Moment>);
+      } else {
+        const prevTxDay = new Date(transactions[index-1].timestamp).getDay();
+        const thisTxDay = new Date(tx.timestamp).getDay();
+
+        if (prevTxDay !== thisTxDay) {
+          items.push(<Moment unix key={index} format="MMMM D, YYYY">{tx.timestamp / 1000}</Moment>);
+        }
+      }
+
+      items.push((
+        <div className={classes.txItem} key={tx.id}>
+          <Transaction tx={tx}/>
+        </div>
+      ));
+    }
+
+    history = items;
   } else {
-    history = <Spinner />
+    history = <Spinner/>
   }
 
   return (
--- frontend/src/components/History/Transaction/Transaction.js
+++ frontend/src/components/History/Transaction/Transaction.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Moment from 'react-moment';
+import { Card, CardContent, Typography, makeStyles } from '@material-ui/core';
 import styles from './Transaction.module.scss';
+
+const useStyles = makeStyles({
+  content: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+});
 
 const Transaction = ({ tx }) => {
   const txInfoStyles = [styles.info];
   const amountStyles = [];
+  const classes = useStyles();
 
   if (tx.status === 'failed') {
     txInfoStyles.push(styles.error);
@@ -11,12 +22,22 @@ const Transaction = ({ tx }) => {
   }
 
   return (
-    <tr className={styles.text}>
-      <td className={styles.date}>{convertTimestamp(tx.timestamp)}</td>
-      <td>{tx.transferType}</td>
-      <td><div className={txInfoStyles.join(' ')}>{getInfoText(tx)}</div></td>
-      <td style={{ textAlign: "right" }} className={amountStyles.join(' ')}>{getAmountText(tx)}</td>
-    </tr>
+    <Card>
+      <CardContent className={classes.content}>
+        <Typography variant="body2" component="span">
+          <Moment unix format="LT">{tx.timestamp / 1000}</Moment>
+        </Typography>
+        <Typography variant="body2" component="span">
+        {tx.transferType}
+        </Typography>
+        <Typography variant="body2" component="span">
+          {getInfoText(tx)}
+        </Typography>
+        <Typography variant="body2" component="span">
+          {getAmountText(tx)}
+        </Typography>
+      </CardContent>
+    </Card>
   );
 }
 
@@ -50,32 +71,6 @@ function getAmountText(tx) {
   text += `${((tx.amount + tx.fee) / 100).toFixed(2)} TRTL`;
 
   return text;
-}
-
-function convertTimestamp(timestamp) {
-  var d = new Date(timestamp),
-      yyyy = d.getFullYear(),
-      mm = ('0' + (d.getMonth() + 1)).slice(-2),  // Months are zero based. Add leading 0.
-      dd = ('0' + d.getDate()).slice(-2),         // Add leading 0.
-      hh = d.getHours(),
-      h = hh,
-      min = ('0' + d.getMinutes()).slice(-2),     // Add leading 0.
-      ampm = 'AM',
-      time;
-
-  if (hh > 12) {
-      h = hh - 12;
-      ampm = 'PM';
-  } else if (hh === 12) {
-      h = 12;
-      ampm = 'PM';
-  } else if (hh === 0) {
-      h = 12;
-  }
-
-  // ie: 2014-03-24, 3:00 PM
-  time = yyyy + '-' + mm + '-' + dd + ', ' + h + ':' + min + ' ' + ampm;
-  return time;
 }
 
 export default Transaction;

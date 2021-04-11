47a8950f9a915f3424fe563fd94896afa9c68cbc
Jacques Jordaan <5298218+zoidbergZA@users.noreply.github.com>
2021-04-11 21:55:38 +0200

    comment out unused imports and vars

--- frontend/src/components/Start.js
+++ frontend/src/components/Start.js
@@ -1,34 +1,34 @@
 import React, { useContext } from 'react';
-import { signInWithRedirect } from '../base';
-import * as firebase from 'firebase/app';
+// import { signInWithRedirect } from '../base';
+// import * as firebase from 'firebase/app';
 import { AuthContext } from '../contexts/Auth';
-import Button from 'react-bulma-components/lib/components/button';
+// import Button from 'react-bulma-components/lib/components/button';
 import Heading from 'react-bulma-components/lib/components/heading';
 import Section from 'react-bulma-components/lib/components/section';
 import Container from 'react-bulma-components/lib/components/container';
 import { Redirect } from 'react-router';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+// import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import logo from '../assets/logo-large.png';
 
 const Start = ({ history, email, github }) => {
   const { currentUser } = useContext(AuthContext);
 
-  const emailLoginHandler = () => {
-    history.push('/login');
-  }
+  // const emailLoginHandler = () => {
+  //   history.push('/login');
+  // // }
 
-  const githubLoginHandler = async () => {
-    var provider = new firebase.auth.GithubAuthProvider();
+  // const githubLoginHandler = async () => {
+  //   var provider = new firebase.auth.GithubAuthProvider();
 
-    try {
-      await signInWithRedirect(provider);
-      history.push('/');
-    } catch (error) {
-      console.error(`${error.errorCode} :: ${error.errorMessage}`);
-    }
-  }
+  //   try {
+  //     await signInWithRedirect(provider);
+  //     history.push('/');
+  //   } catch (error) {
+  //     console.error(`${error.errorCode} :: ${error.errorMessage}`);
+  //   }
+  // }
 
   if (currentUser) {
     return <Redirect to="/" />;
@@ -40,14 +40,14 @@ const Start = ({ history, email, github }) => {
     alignItems: "center"
   }
 
-  const btnStyle = {
-    width: "250px",
-    marginTop: "10px"
-  }
+  // const btnStyle = {
+  //   width: "250px",
+  //   marginTop: "10px"
+  // }
 
-  const btnText = {
-    paddingLeft: "10px"
-  }
+  // const btnText = {
+  //   paddingLeft: "10px"
+  // }
 
   // return (
   //   <Section>
